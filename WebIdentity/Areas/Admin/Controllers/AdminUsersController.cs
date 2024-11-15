16f77eb7b1f82f1cfad2a5d81ebe6436911c68ad
Joao Guilhermem <joaoguilhermeoboista@gmail.com>
2024-11-15 16:47:05 -0300

    Parte 4

---
+++ WebIdentity/Areas/Admin/Controllers/AdminUsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebIdentity.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminUsersController : Controller
+    {
+        private  readonly UserManager<IdentityUser> _userManager;
+
+        public AdminUsersController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult index()
+        {
+            var user = _userManager.Users;
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>DeletarUser(string Id)
+        {
+            var user = await _userManager.FindByNameAsync(Id);
+
+            if (user == null)
+            {
+                ViewBag.ErrorMennssage = $"Usuario com id ={Id} não foi encontrodo";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await _userManager.DeleteAsync(user);
+
+                if (result.Succeeded) 
+                    return RedirectToAction("Index");
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+
+                return View("Index");
+            }
+        }
+    }
+}
